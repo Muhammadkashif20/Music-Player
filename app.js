332edacdc4ce393f609275b92c0b39f4af4d9bc0
muhammadkashif20 <kashif28@gmail.com>
2024-07-31 18:37:06 +0500

    Media Query Working

--- app.js
+++ app.js
@@ -2,31 +2,36 @@
 let start = document.getElementById("playbtn");
 let pause = document.getElementById("pausebtn");
 let audio = document.getElementById("audio");
-let images=document.getElementById('img')
-let naat1=document.getElementById('naat1')
-let naat2=document.getElementById('naat2')
-let naat3=document.getElementById('naat3')
-let naat4=document.getElementById('naat4')
+let images = document.getElementById("img");
+let naat1 = document.getElementById("naat1");
+let naat2 = document.getElementById("naat2");
+let naat3 = document.getElementById("naat3");
+let naat4 = document.getElementById("naat4");
 // let range = document.querySelector(".range");
 let startStop = document.getElementById("Center-arrow");
-let count=0
-let playAudio=audio.innerHTML+=['audio/naat1.mp3','audio/naat2.mp3','audio/naat3.mp3','audio/naat4.mp3']
+let count = 0;
+let playAudio = (audio.innerHTML += [
+  "audio/naat1.mp3",
+  "audio/naat2.mp3",
+  "audio/naat3.mp3",
+  "audio/naat4.mp3",
+]);
 // console.log(playAudio);
 startStop.addEventListener("click", () => {
-  playAudio.src=playAudio[count]
+  playAudio.src = playAudio[count];
   if (start.style.display == "none") {
     start.style.display = "block";
     pause.style.display = "none";
-    audio.pause()
+    audio.pause();
   } else {
     start.style.display = "none";
     pause.style.display = "block";
-   audio.play()
+    audio.play();
   }
 });
 let previous = document.getElementById("left-arrow");
 let next = document.getElementById("right-arrow");
-previous.addEventListener('click',()=>{
+previous.addEventListener("click", () => {
   // if(naat1.style.display=='none'){
   //   naat1.style.display='block'
   //   audio.pause()
@@ -35,11 +40,10 @@ previous.addEventListener('click',()=>{
   //   naat1.style.display='none'
   //   playAudio.src=playAudio[count]
   //   audio.play()
-
   // }
-})
-next.addEventListener('click',()=>{
-  playAudio.src=playAudio[count]
+});
+next.addEventListener("click", () => {
+  playAudio.src = playAudio[count];
   // if(audio.play()){
   //   audio.play()
   // }
@@ -47,5 +51,4 @@ next.addEventListener('click',()=>{
   // audio.pause()
 
   // }
-})
-
+});
